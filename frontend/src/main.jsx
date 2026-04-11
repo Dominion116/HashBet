11c5930fee012e2eff5d7e38d3b76c04ef1eb4a7
Oyewale Dominion <limbotech116@gmail.com>
2026-04-11 18:15:40 +0100

    chore(frontend): add React entrypoint

---
+++ frontend/src/main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import HashBetMini from "./App";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <HashBetMini />
+  </React.StrictMode>
+);
